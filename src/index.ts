cce284ff0fd3b20befc48da1918a9cbd1c963526
Tomáš Kazda <tomas.kazda@pslib.cz>
2025-11-05 11:54:44 +0100

    hodina 251105 deklarace

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//let zahájí deklaraci proměnné s blokovým rozsahem
+//následuje název proměnné 
+//za dvojtečkou její typ (číslo, text, boolean, atd.)
+let prvniCislo: number; //toto je deklarace proměnné s hodnotou undefined
+let druheCislo: number = 5; //toto je deklarace i inicializace proměnné
+// = je operátor přiřazení hodnoty
+prvniCislo = 10; //přiřazení hodnoty do již deklarované proměnné
+
+//deklarace konstanty
+const tretiCislo: number = 15; //konstanty musí být inicializovány při deklaraci
+//tretiCislo = 15; //toto by vyvolalo chybu
+
+//proměnné mohou být různých typů
+let textovaPromenna: string = "Ahoj světe!"; //toto je textová proměnná
+let logickaPromenna: boolean = true; //toto je logická proměnná
