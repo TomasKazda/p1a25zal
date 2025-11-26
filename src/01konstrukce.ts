2c4d409bdaf6d6a899a896506f04e0bf3cd293b1
Tomáš Kazda <tomas.kazda@pslib.cz>
2025-11-26 11:48:41 +0100

    hodina 251126 while, for

--- src/01konstrukce.ts
+++ src/01konstrukce.ts
@@ -1,4 +1,7 @@
 // podmíněné vykonávání
+
+import { join } from "node:path";
+
 // výraz musí být boolean, nebo konvertovatelný na boolean
 let expression: boolean = 5 > 2;
 if (expression) {
@@ -25,9 +28,32 @@ if (exp1) {
     console.log("takhle to děláme :-)")
 }
 
-
+let necoDuleziteho: string = "ahoj";
 //podmíněné cyklické vykonávání
 while (exp1) {
     //iterace cyklu
     //kód se vykonává několikrát - dokud je exp1 true
+    //aby cyklus neskončil, 
+    //  musí se někde uvnitř změnit hodnota proměnné použitá v podmínce
+    exp1 = false; // jinak by to byl nekonečný cyklus
 }
+
+do {
+    let necoDuleziteho: string = "čau";
+    //tento kód se vykoná alespoň jednou
+} while (exp1);
+console.log(necoDuleziteho)
+
+let i: number = 0;
+while (i < 5) {
+    console.log(`i je nyní: ${i}`);
+    i++; //inkrementace
+}
+
+//stejná jako while, ale řídicí proměnná vzniká (a zaniká) 
+// přímo v konstrukci for
+for (let j: number = 0; j < 5; j++) {
+    console.log(`j je nyní: ${j}`);
+}
+console.log(i);
+//console.log(j); // chyba, j není definováno
